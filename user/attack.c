9d067549f885a1e12e9d4ad64e8c0112e3fecc7a
JakobMusik <ovonwn@gmail.com>
2025-06-28 15:49:41 +0800

    lab2 done

--- user/attack.c
+++ user/attack.c
@@ -3,11 +3,26 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
+/**
+ * solution: https://www.youtube.com/watch?v=8wq1BcXhjp4
+ */
+
 int
 main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
+  
+  if(argc != 1){
+    printf("Usage: attack\n");
+    exit(1);
+  }
+  char* end = sbrk(PGSIZE * 17);
+  // end = end + 9 * PGSIZE;
+  end = end + 16 * PGSIZE;
+  strcpy(end, " ");
+  char* secret = end + 32;
+  write(2, secret, 8);
 
   exit(1);
 }
